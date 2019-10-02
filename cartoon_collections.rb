a6c90e9a179b738403819cdf2c78acbae1bd0c68
Florian Stagliano <contact.f.stagliano@gmail.com>
2019-10-02 10:13:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -30,10 +30,10 @@ def find_the_cheese(array_of_strings)
   array_of_strings.each do |string| 
     cheese_types.each do |cheese|  
       if string.include?(cheese)
-        output << string 
-        return output
+        output << string;
+        return output;
       else
-        return nil
+        return nil;
       end
     end
   end
