6c4125edbdc9c873c687bc96e63bc295f3c21998
Florian Stagliano <contact.f.stagliano@gmail.com>
2019-10-02 09:37:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ end
 def summon_captain_planet(array_planeteer)
   planeteer_calls=[]
   
-  planeteer_calls=array_planeteer.map {|planeteer| "#{planeteer.capitalize}!"
+  planeteer_calls=array_planeteer.map {|planeteer| "#{planeteer.capitalize}!"}
 end
 
 def long_planeteer_calls# code an argument here
