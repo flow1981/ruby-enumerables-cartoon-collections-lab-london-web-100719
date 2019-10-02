e7da387928bd8c5d483560a882f4cfe3d1ed51e7
Florian Stagliano <contact.f.stagliano@gmail.com>
2019-10-02 09:48:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,7 @@ def long_planeteer_calls(array_calls)
   
 end
 
-def find_the_cheese(array_of_cheese)
-  
+def find_the_cheese(array_of_strings)
+  {|string| 
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
