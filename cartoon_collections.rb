9498eef0a8b24bb6ed825289d36c848f26539762
Florian Stagliano <contact.f.stagliano@gmail.com>
2019-10-02 09:30:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,6 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array_dwarfs)
+  array_dwarfs.each_with_index {|dwarf| puts }
+  
 end
 
 def summon_captain_planet# code an argument here
